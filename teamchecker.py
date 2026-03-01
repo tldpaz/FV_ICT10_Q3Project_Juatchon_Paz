a8f630b16ddf97bffe1a27896b04748bde02b047
(paz)erwort <cheesylemon9010@gmail.com>
2026-03-02 03:46:38 +0800

    Refactor check_team function for clarity and comments

--- teamchecker.py
+++ teamchecker.py
@@ -1,33 +1,35 @@
 from pyscript import document, when
 
-@when("click", "#checkBtn")
+# This function runs when the "Check Team" button is clicked
+@when("click", "#checkBtn") #cred to gpt idk what this does but before i used this my page was wonky so apparently it's called a decorator for the browser thing but im unfamiliar with this honestly
 def check_team(event):
 
+    # Get selected radio button values
     registration = document.querySelector('input[name="registration"]:checked')
     medical = document.querySelector('input[name="medical"]:checked')
 
+    # Get grade input value
     grade_value = document.getElementById("grade").value
     output = document.getElementById("teamOutput")
 
-    # ya make sure that shit is answered yow
+    # Check if required questions were answered
     if registration is None or medical is None:
         output.innerText = "Please answer all questions."
         return
 
-    # yay
+    # Check if registration and medical clearance are both "yes"
     if registration.value != "yes" or medical.value != "yes":
         output.innerText = "You must complete registration and medical clearance first."
         return
 
-    # yay
+    # Convert grade input into integer
     try:
         grade = int(grade_value)
     except:
         output.innerText = "Please select your grade."
         return
 
-    # yip
-    
+    # Assign team based on grade level
     if grade == 7:
         team = "Yellow Tigers"
     elif grade == 8:
@@ -36,9 +38,6 @@ def check_team(event):
         team = "Red Bulldogs"
     elif grade == 10:
         team = "Green Hornets"
-    else:
-        team = "No team assigned"
 
-    
-
-    output.innerText = f"Congratulations! You are part of the {team}"
+    # Display final result
+    output.innerText = f"Congratulations! You are part of the {team}!"
